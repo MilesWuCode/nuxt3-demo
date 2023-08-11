692c1569542f3f347c763e1567a27dff1960f1a4
Miles Wu <slion0725@gmail.com>
2023-08-11 16:39:06 +0800

    upd

--- components/MainMenu.vue
+++ components/MainMenu.vue
@@ -3,12 +3,22 @@ const menu = [
   { name: 'Index', href: '/' },
   { name: 'Login', href: '/login' },
 ]
+
+const hideMenu = () => {
+  const drawerCheckbox = document.getElementById(
+    'layout-default-drawer',
+  ) as HTMLInputElement | null
+
+  if (drawerCheckbox !== null) {
+    drawerCheckbox.checked = false
+  }
+}
 </script>
 
 <template>
   <ul class="menu menu-md bg-base-100 h-full w-80 p-4">
     <li v-for="item in menu" :key="item.name">
-      <NuxtLink :to="item.href">{{ item.name }}</NuxtLink>
+      <NuxtLink :to="item.href" @click="hideMenu">{{ item.name }}</NuxtLink>
     </li>
   </ul>
 </template>
