bdfee3fe42a4e549acc467873d565ea7c3930282
Miles Wu <slion0725@gmail.com>
2023-08-11 11:07:25 +0800

    layout + page

--- app.vue
+++ app.vue
@@ -1,5 +1,11 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  <Html lang="zh-TW">
+    <Body>
+      <NuxtLayout>
+        <!-- 進度條 -->
+        <NuxtLoadingIndicator />
+        <NuxtPage />
+      </NuxtLayout>
+    </Body>
+  </Html>
 </template>
