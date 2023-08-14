bd80abd0f0eafc3e68b4e7ecf3ce5b95c0998f44
Miles Wu <slion0725@gmail.com>
2023-08-14 14:05:19 +0800

    upd

---
+++ components/LoginForm.vue
@@ -0,0 +1,111 @@
+<script setup lang="ts">
+import { InvalidSubmissionContext, useField, useForm } from 'vee-validate'
+
+import { toTypedSchema } from '@vee-validate/zod'
+
+import * as zod from 'zod'
+
+const validationSchema = toTypedSchema(
+  zod.object({
+    email: zod
+      .string()
+      .nonempty('This is required')
+      .email({ message: 'Must be a valid email' }),
+    password: zod
+      .string()
+      .nonempty('This is required')
+      .min(8, { message: 'Too short' }),
+  }),
+)
+
+const { handleSubmit, errors, setFieldError, setErrors } = useForm({
+  validationSchema,
+})
+const { value: email } = useField('email')
+const { value: password } = useField('password')
+
+const onSubmit = handleSubmit((values) => {
+  alert(JSON.stringify(values, null, 2))
+}, onInvalidSubmit)
+
+function onInvalidSubmit({
+  values,
+  errors,
+  results,
+}: InvalidSubmissionContext) {
+  // 欄位名
+  const name = Object.keys(errors)[0]
+
+  // focus
+  document.getElementsByName(name)[0].focus()
+
+  console.log(values) // current form values
+  console.log(errors) // a map of field names and their first error message
+  console.log(results) // a detailed map of field names and their validation results
+}
+</script>
+
+<template>
+  <form @submit="onSubmit">
+    <!-- 欄位 -->
+    <div class="form-control">
+      <label class="label">
+        <span class="label-text" :class="errors.email && 'text-error'">
+          Email address
+        </span>
+        <span class="label-text-alt"></span>
+      </label>
+
+      <input
+        v-model="email"
+        label="信箱"
+        name="email"
+        type="text"
+        class="input input-bordered"
+        :class="errors.email && 'input-error'"
+      />
+
+      <label class="label">
+        <span class="label-text-alt text-error">{{ errors.email }}</span>
+        <span class="label-text-alt"></span>
+      </label>
+    </div>
+    <!-- 欄位 -->
+    <div class="form-control">
+      <label class="label">
+        <span class="label-text" :class="errors.password && 'text-error'">
+          Password
+        </span>
+        <span class="label-text-alt"></span>
+      </label>
+
+      <input
+        v-model="password"
+        label="密碼"
+        name="password"
+        type="password"
+        class="input input-bordered"
+        :class="errors.password && 'input-error'"
+      />
+
+      <label class="label">
+        <span class="label-text-alt text-error">{{ errors.password }}</span>
+        <span class="label-text-alt"></span>
+      </label>
+    </div>
+
+    <!-- 按鈕 -->
+    <div class="flex flex-col space-y-2">
+      <button type="submit" class="btn btn-primary">Submit</button>
+
+      <div class="flex justify-between space-x-2 text-sm">
+        <NuxtLink to="/register" class="link-primary link no-underline">
+          Register Now
+        </NuxtLink>
+        <NuxtLink to="/forgot-password" class="link no-underline">
+          Forgot Password
+        </NuxtLink>
+      </div>
+    </div>
+  </form>
+</template>
--- pages/login.vue
+++ pages/login.vue
@@ -7,54 +7,7 @@
     <div class="flex justify-center">
       <div class="w-full max-w-sm">
         <!-- 表單 -->
-        <form>
-          <!-- 欄位 -->
-          <div class="form-control">
-            <label class="label">
-              <span class="label-text">Email address</span>
-              <span class="label-text-alt"></span>
-            </label>
-            <input
-              type="text"
-              placeholder="Type here"
-              class="input input-bordered"
-            />
-            <label class="label">
-              <span class="label-text-alt text-error">Bottom Left label</span>
-              <span class="label-text-alt"></span>
-            </label>
-          </div>
-          <!-- 欄位 -->
-          <div class="form-control">
-            <label class="label">
-              <span class="label-text">Password</span>
-              <span class="label-text-alt"></span>
-            </label>
-            <input
-              type="text"
-              placeholder="Type here"
-              class="input input-bordered input-error"
-            />
-
-            <label class="label">
-              <span class="label-text-alt text-error">Bottom Left label</span>
-              <span class="label-text-alt"></span>
-            </label>
-          </div>
-          <!-- 按鈕 -->
-          <div class="flex flex-col space-y-2">
-            <button type="submit" class="btn btn-primary">Submit</button>
-
-            <div class="flex justify-between space-x-2 text-sm">
-              <NuxtLink to="/register" class="link-primary link no-underline">
-                Register Now
-              </NuxtLink>
-              <NuxtLink to="/forgot-password" class="link no-underline">
-                Forgot Password
-              </NuxtLink>
-            </div>
-          </div>
-        </form>
+        <LoginForm />
 
         <!-- 水平線 -->
         <div class="divider">OR</div>
