2fdddd52b7384d98df21af80e476293e44f663ff
Miles Wu <slion0725@gmail.com>
2023-08-14 11:53:31 +0800

    upd

--- pages/login.vue
+++ pages/login.vue
@@ -1,5 +1,9 @@
 <template>
-  <div class="container">
+  <div class="container m-2">
+    <!-- 標題 -->
+    <h1 class="flex justify-center text-2xl">Login</h1>
+
+    <!-- 區塊 -->
     <div class="flex justify-center">
       <div class="w-full max-w-sm">
         <!-- 表單 -->
