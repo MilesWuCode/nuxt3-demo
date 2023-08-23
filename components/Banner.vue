e7d524cb745b9bf03ad3c288ecdb7426eb0022aa
Miles Wu <slion0725@gmail.com>
2023-08-23 17:51:56 +0800

    upd

--- components/Banner.vue
+++ components/Banner.vue
@@ -1,14 +1,8 @@
 <template>
   <picture class="w-full">
-    <source
-      srcset="http://localhost:3000/images/768x384.png"
-      media="(min-width: 768px)"
-    />
-    <source
-      srcset="http://localhost:3000/images/640x320.png"
-      media="(min-width: 640px)"
-    />
+    <source srcset="/images/768x384.png" media="(min-width: 768px)" />
+    <source srcset="/images/640x320.png" media="(min-width: 640px)" />
     <!-- 最小 -->
-    <img src="http://localhost:3000/images/320x160.png" width="100%" />
+    <img src="/images/320x160.png" width="100%" />
   </picture>
 </template>
