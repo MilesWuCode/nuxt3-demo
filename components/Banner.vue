da8db1bc86e0f01813613a3eee142ea7cc552fdc
Miles Wu <slion0725@gmail.com>
2023-10-03 13:57:51 +0800

    upd

--- components/Banner.vue
+++ components/Banner.vue
@@ -1,9 +1,9 @@
 <!-- 換頁時有問題 -->
 <template>
   <picture>
-    <source srcset="/img/c2.jpg" media="(min-width: 768px)" />
-    <source srcset="/img/c3.jpg" media="(min-width: 640px)" />
+    <source srcset="" media="(min-width: 768px)" />
+    <source srcset="" media="(min-width: 640px)" />
     <!-- 最小 -->
-    <img src="/img/c1.jpg" width="1000" />
+    <img src="" width="1000" />
   </picture>
 </template>
