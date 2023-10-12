1ec1ef036be73d03029e50f4d0254840a574aa88
Miles Wu <slion0725@gmail.com>
2023-10-12 17:55:06 +0800

    upd

--- pages/post.vue
+++ pages/post.vue
@@ -4,10 +4,8 @@
     <h1 class="flex justify-center text-2xl">Post</h1>
 
     <!-- 區塊 -->
-
-    <Banner />
-
     <div class="flex justify-center">
+      <Banner />
       <!-- <PostCard /> -->
     </div>
   </div>
