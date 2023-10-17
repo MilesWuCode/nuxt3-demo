8fe2da81b0b4d1c457e9bb19e0990683d452a72f
Miles Wu <slion0725@gmail.com>
2023-10-17 10:58:03 +0800

    upd

--- server/api/post/index.get.ts
+++ server/api/post/index.get.ts
@@ -1,14 +1,15 @@
 import { PrismaClient } from '@prisma/client'
 import { getServerSession } from '#auth'
 
+// 建議放在這裡
+const prisma = new PrismaClient()
+
 export default defineEventHandler(async (event) => {
   const session = await getServerSession(event)
 
   if (!session) {
     throw createError({ status: 401, statusMessage: 'Unauthorized' })
   }
-
-  const prisma = new PrismaClient()
 
   const data = await prisma.user
     .findUnique({ where: { id: session.user.id } })
