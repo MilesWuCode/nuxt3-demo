22d68e809c3efd45295acb9a290aa554ae45306c
Miles Wu <slion0725@gmail.com>
2023-10-17 10:33:12 +0800

    fix

--- server/api/post/index.get.ts
+++ server/api/post/index.get.ts
@@ -1,14 +1,14 @@
 import { PrismaClient } from '@prisma/client'
 import { getServerSession } from '#auth'
 
-const prisma = new PrismaClient()
-
 export default defineEventHandler(async (event) => {
   const session = await getServerSession(event)
 
   if (!session) {
     return { status: 'unauthenticated!' }
   }
+
+  const prisma = new PrismaClient()
 
   const posts = await prisma.user
     .findUnique({ where: { id: session.user.id } })
