faa047d2fcd7e9ea8ec0e4855213b12fd5af0414
Miles Wu <slion0725@gmail.com>
2023-11-20 14:55:14 +0800

    upd

--- components/FavoriteButton.vue
+++ components/FavoriteButton.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
 const isFavorite = ref(false)
+const loading = ref(false)
 </script>
 
 <template>
-  <label class="swap">
+  <span v-if="loading" class="loading loading-spinner loading-sm"></span>
+
+  <label v-else class="swap">
     <input v-model="isFavorite" type="checkbox" />
     <div class="swap-off"><Icon name="IconHeartOutline" /></div>
     <div class="swap-on"><Icon name="IconHeartSolid" /></div>
