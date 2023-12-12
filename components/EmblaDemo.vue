777df8f3498b5c3aa32bf87172b4003ad8084e7f
Miles Wu <slion0725@gmail.com>
2023-12-12 16:54:34 +0800

    upd

--- components/EmblaDemo.vue
+++ components/EmblaDemo.vue
@@ -3,25 +3,90 @@ import emblaCarouselVue from 'embla-carousel-vue'
 import Autoplay from 'embla-carousel-autoplay'
 
 const [emblaNode, emblaApi] = emblaCarouselVue(
-  { loop: false, dragFree: true, align: 'start' },
-  [Autoplay({ delay: 3000 })],
+  {
+    loop: false,
+    dragFree: true,
+    align: 'start',
+    slidesToScroll: 2,
+  },
+  [
+    /* Autoplay({ delay: 3000 }) */
+  ],
 )
+
+const list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
+
+const dots = computed(() => emblaApi.value?.scrollSnapList() || [])
+
+const canPrev = ref(false)
+
+const canNext = ref(false)
+
+const scrollIndex = ref<number>(0)
 
 watchEffect(() => {
   if (emblaApi.value) {
-    console.log(emblaApi.value.slideNodes()) // Access API
+    emblaApi.value.on('select', onSelect)
+
+    onSelect()
   }
 })
 
-const list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+const onSelect = () => {
+  if (emblaApi.value) {
+    scrollIndex.value = emblaApi.value.selectedScrollSnap()
+    canPrev.value = emblaApi.value?.canScrollPrev() || false
+    canNext.value = emblaApi.value?.canScrollNext() || false
+  }
+}
+
+const scrollTo = (index: number) => {
+  emblaApi.value && emblaApi.value.scrollTo(index)
+}
+
+const onPrev = () => {
+  emblaApi.value && emblaApi.value.scrollPrev()
+}
+const onNext = () => {
+  emblaApi.value && emblaApi.value.scrollNext()
+}
 </script>
 
 <template>
-  <div ref="emblaNode" class="embla">
-    <div class="embla__container">
-      <div v-for="idx of list" :key="idx" class="embla__slide">
-        <img :src="`https://fakeimg.pl/400x300/?text=${idx}`" />
+  <div class="relative">
+    <div ref="emblaNode" class="embla">
+      <div class="embla__container">
+        <div v-for="idx of list" :key="idx" class="embla__slide">
+          <img :src="`https://fakeimg.pl/400x300/?text=${idx}`" />
+        </div>
       </div>
+    </div>
+    <div class="absolute left-[100px] top-[100px]">
+      <button
+        class="btn btn-lg z-[1]"
+        :disabled="!canPrev || scrolling"
+        @click="onPrev"
+      >
+        Prev
+      </button>
+      <button
+        class="btn btn-lg z-[1]"
+        :disabled="!canNext || scrolling"
+        @click="onNext"
+      >
+        Next
+      </button>
+    </div>
+    <div class="space-x-4">
+      <button
+        v-for="(_, idx) of dots"
+        :key="idx"
+        class="btn btn-xs"
+        :class="{ 'btn-info': idx === scrollIndex }"
+        @click="scrollTo(idx)"
+      >
+        {{ idx }}
+      </button>
     </div>
   </div>
 </template>
@@ -29,6 +94,7 @@ const list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 <style scoped>
 .embla {
   overflow: hidden;
+  width: 1200px;
 }
 .embla__container {
   display: flex;
